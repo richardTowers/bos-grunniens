a9db0547ed87b3e7242aff1c84d730c411306cd8
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-04-20 13:33:38 +0100

    Hello static files!

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,8 +3,11 @@
 module Lib ( helloWorld ) where
 
 import Web.Scotty
+import Network.Wai.Middleware.Static
 
 helloWorld :: IO ()
-helloWorld = scotty 3000 $
-  get "/:word" $
+helloWorld = scotty 3000 $ do
+  middleware $ staticPolicy $ noDots >-> isNotAbsolute >-> hasPrefix "public"
+
+  get "/" $
     html "<h1>Hello World!</h1>"
